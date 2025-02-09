af55ff888bc229e9637afe323e9b8ae74a9863db
aliciariveraa <aliciarivera@usf.edu>
2025-02-09 18:49:42 -0500

    Task 2 - Order Details

--- cc_5.js
+++ cc_5.js
@@ -4,7 +4,20 @@ let customer = {
     age: 35,
     email: "john.doe@email.com"
 };
-
 console.log(`Customer Name: ${customer.name}`);
 console.log(`Customer Age: ${customer.age}`);
 console.log(`Customer Email: ${customer.email}`);
+
+// Task 2 - Order Details
+let order = {
+    orderId: 12345,
+    totalAmount: 150,
+    status: "Processing",
+    displayOrder: function() {
+        console.log(`Order ID: ${this.orderId}`);
+        console.log(`Total Amount: ${this.totalAmount}`);
+        console.log(`Status: ${this.status}`);
+    }
+};
+
+order.displayOrder();
